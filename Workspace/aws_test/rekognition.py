c3429f71f3e2cee3942b9c34cef2e61dfe98ce35
Jeganath PV <jeganathpv@gmail.com>
2020-01-11 17:13:49 +0530

    Testing with AWS Detect Labels

---
+++ Workspace/aws_test/rekognition.py
@@ -0,0 +1,20 @@
+import csv
+import boto3
+
+with open('credentials.csv','r') as input:
+    next(input)
+    reader = csv.reader(input)
+    for line in reader:
+        access_key_id = line[2]
+        secret_access_key = line[3]
+
+photo  = 'jegan.jpg'
+
+client = boto3.client('rekognition',aws_access_key_id = access_key_id, aws_secret_access_key = secret_access_key, region_name='us-west-2')
+
+with open(photo,'rb') as source_image:
+    source_bytes = source_image.read()
+
+response = client.detect_labels(Image = {'Bytes':source_bytes},MaxLabels = 10)
+
+print(response)
