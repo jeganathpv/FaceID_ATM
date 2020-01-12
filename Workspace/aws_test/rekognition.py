732aa2ed6d593144193528c4cf9d995092f14b16
Jeganath PV <jeganathpv@gmail.com>
2020-01-12 15:56:02 +0530

    Added Region in boto3 client

--- Workspace/aws_test/rekognition.py
+++ Workspace/aws_test/rekognition.py
@@ -7,10 +7,11 @@ with open('credentials.csv','r') as input:
     for line in reader:
         access_key_id = line[2]
         secret_access_key = line[3]
+        region = line[5]
 
 photo  = 'jegan.jpg'
 
-client = boto3.client('rekognition',aws_access_key_id = access_key_id, aws_secret_access_key = secret_access_key, region_name='us-west-2')
+client = boto3.client('rekognition',aws_access_key_id = access_key_id, aws_secret_access_key = secret_access_key, region_name=region)
 
 with open(photo,'rb') as source_image:
     source_bytes = source_image.read()
