444937b802e5d0189a10603b4452906b576817ab
Jeganath PV <jeganathpv@gmail.com>
2020-04-01 17:39:50 +0530

    Final Routing

--- Workspace/NextGenATM/NextGenATM.UI/src/app/app-routing.module.ts
+++ Workspace/NextGenATM/NextGenATM.UI/src/app/app-routing.module.ts
@@ -7,16 +7,6 @@ import { UrlselectorComponent } from './urlselector/urlselector.component';
 import { AtmFlowComponent } from './atm-flow/atm-flow.component';
 
 
-// for admin 
-// const routes: Routes = [
-//   { path: '', component: UrlselectorComponent , pathMatch:'full'},
-//   { path: 'enrollment', component: EnrollmentComponent  , pathMatch:'full' },
-//   { path: 'enroll', component: UrlselectorComponent  , pathMatch:'full'},
-//   { path: 'atm-flow',component: AtmFlowComponent , pathMatch:'full'}
-// ];
-
-
-// for atm 
 const routes: Routes = [
   { path: '', component: UrlselectorComponent , pathMatch:'full'},
   { path: 'enroll', component: UrlselectorComponent  , pathMatch:'full'},
