f6f535d2fe330bbb8703d4037328e1d2d68b40bf
Jeganath PV <jeganathpv@gmail.com>
2020-04-02 12:14:32 +0530

    Formatted and remove unneccessary files

--- Workspace/NextGenATM/NextGenATM.Services/EnrollmentAPI.py
+++ Workspace/NextGenATM/NextGenATM.Services/EnrollmentAPI.py
@@ -2,88 +2,90 @@ from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from flask_cors import CORS, cross_origin
 
-#Import Models,Services
+# Import Models,Services
 from Models import UserObject, CustomerObject, FaceIndexObject
-from Services import  LoginUser, BankService, AccountService, FaceDetection, FaceID
+from Services import LoginUser, BankService, AccountService, FaceDetection, FaceID
 from StaticStatus import FaceIndex
 
 app = Flask(__name__)
-CORS(app, support_credentials = True)
+CORS(app, support_credentials=True)
 api = Api(app)
 
+
 class HealthChecker(Resource):
-    @app.route('/checkhealth',methods = ['GET'])
-    @cross_origin(support_credentials = True)
+    @app.route('/checkhealth', methods=['GET'])
+    @cross_origin(support_credentials=True)
     def checkHealth(self):
         return jsonify(True)
 
+
 class LoginController(Resource):
-    @app.route('/auth',methods = ['POST'])
-    @cross_origin(support_credentials = True)
+    @app.route('/auth', methods=['POST'])
+    @cross_origin(support_credentials=True)
     def userLogin(self):
-        json_data = request.get_json(force = True)
+        json_data = request.get_json(force=True)
         username = str(json_data['username'])
         password = str(json_data['password'])
-        userObj = UserObject(username,password)
-        return jsonify({'Status':str(LoginUser.validateUser(userObj))})
+        userObj = UserObject(username, password)
+        return jsonify({'Status': str(LoginUser.validateUser(userObj))})
+
 
 class BankController(Resource):
-    @app.route('/bank/getdetails',methods = ['GET'])
-    @cross_origin(support_credentials = True)
+    @app.route('/bank/getdetails', methods=['GET'])
+    @cross_origin(support_credentials=True)
     def getBankDetails(self):
-        return jsonify({"BankDetails":BankService.getBankDetails()})
+        return jsonify({"BankDetails": BankService.getBankDetails()})
+
 
 class AccountController(Resource):
-    @app.route('/account/generatecustid',methods = ['POST'])
-    @cross_origin(support_credentials = True)
+    @app.route('/account/generatecustid', methods=['POST'])
+    @cross_origin(support_credentials=True)
     def generateCustId(self):
         custId = AccountService.generateCustomerID()
-        json_data = request.get_json(force = True)
+        json_data = request.get_json(force=True)
         branchCode = json_data['branchCode']
         accountNo = json_data['accountNo']
         name = json_data['name']
         balance = json_data['balance']
-        custObj = CustomerObject(custId,branchCode,accountNo,name,balance)
+        custObj = CustomerObject(custId, branchCode, accountNo, name, balance)
         accountNo = AccountService.addCustomerDetails(custObj)
-        AccountService.updateExistingDetails(custId[-3:],branchCode,accountNo)
-        return jsonify({'Status':True,'customerID':custId})
+        AccountService.updateExistingDetails(
+            custId[-3:], branchCode, accountNo)
+        return jsonify({'Status': True, 'customerID': custId})
 
-    @app.route('/account/generateqrcard',methods = ['POST'])
-    @cross_origin(support_credentials = True)
+    @app.route('/account/generateqrcard', methods=['POST'])
+    @cross_origin(support_credentials=True)
     def generateQRDetails(self):
-        json_data = request.get_json(force =True)
+        json_data = request.get_json(force=True)
         customerID = json_data['customerID']
         qrDetail = AccountService.fetchCustomerDetail(customerID)
         qrCardString = AccountService.generateQrCard(qrDetail)
-        return jsonify({"card":qrCardString})
+        return jsonify({"card": qrCardString})
 
 
 class FaceIDController(Resource):
-    @app.route('/faceid/detectface',methods = ['POST'])
-    @cross_origin(support_credentials = True)
+    @app.route('/faceid/detectface', methods=['POST'])
+    @cross_origin(support_credentials=True)
     def detectFace(self):
-        json_data = request.get_json(force = True)
+        json_data = request.get_json(force=True)
         image_str = json_data['image']
         path = FaceDetection.saveFile(image_str)
         status = FaceDetection.locateFacesInImage(path)
-        return jsonify({'Status':status})
+        return jsonify({'Status': status})
 
-    @app.route('/faceid/indexface',methods = ['POST'])
-    @cross_origin(support_credentials = True)
+    @app.route('/faceid/indexface', methods=['POST'])
+    @cross_origin(support_credentials=True)
     def indexFace(self):
-        json_data = request.get_json(force =True)
+        json_data = request.get_json(force=True)
         customerID = json_data['customerID']
         image_str = json_data['image']
         status = FaceID.index_faces(image_str)
-        if status[0]== FaceIndex.Success:
-            faceIndexObj = FaceIndexObject(customerID,status[1])
+        if status[0] == FaceIndex.Success:
+            faceIndexObj = FaceIndexObject(customerID, status[1])
             FaceID.addIndexinDB(faceIndexObj)
-            return jsonify({'Status':FaceIndex.Success.value})
+            return jsonify({'Status': FaceIndex.Success.value})
         else:
-            return jsonify({'Status':FaceIndex.Failure.value})
-
-
-
+            return jsonify({'Status': FaceIndex.Failure.value})
 
 
 if __name__ == "__main__":
@@ -92,4 +94,4 @@ if __name__ == "__main__":
     api.add_resource(BankController)
     api.add_resource(AccountController)
     api.add_resource(FaceIDController)
-    app.run(host= '0.0.0.0',port=5000)
+    app.run(host='0.0.0.0', port=5000)
--- Workspace/NextGenATM/NextGenATM.Services/StaticStatus.py
+++ Workspace/NextGenATM/NextGenATM.Services/StaticStatus.py
@@ -1,28 +1,33 @@
 import enum
 
+
 class Login(enum.Enum):
     Success = 1
     NotFound = 2
     WrongPassword = 3
+
 
 class FaceCount(enum.Enum):
     Success = 1
     NotFound = 2
     MoreThanOne = 3
 
+
 class FaceIndex(enum.Enum):
     Success = 1
     Failure = 2
+
 
 class QRMatch(enum.Enum):
     AccountFound = 0
     AccountNotFound = 1
 
+
 class FaceIDMatch(enum.Enum):
     FaceIDMatchWithAccount = 1
-    FaceIDNotMatchWithAccount = 2 
+    FaceIDNotMatchWithAccount = 2
+
 
 class CashWithdrawal(enum.Enum):
     Success = 1
     InsufficientBalance = 2
-
